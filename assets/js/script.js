f5b27e35d43951238be7722818f8c88a66953c04
RifdahSyamsu <156346841+RifdahSyamsu@users.noreply.github.com>
2024-01-11 17:34:14 +0000

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".teks", {
+  strings: ["Mahasiswi Semester 3 Jurusan Matematika","Sedang Belajar HTML,CSS,DAN JAVASCRIPT","Akan Menampilkan Tugas Project"],
+  typeSpeed: 60,
+  backSpeed: 60,
+  backDelay: 1000,
+  loop: true,
+});
